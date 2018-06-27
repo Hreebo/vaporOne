a29134e0f262bfc9e7340615d56e1cae6e15ce9b
Lukas Hrebik <hrebik.lukas@gmail.com>
2018-06-27 19:47:41 +0200

    second try

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -28,7 +28,7 @@ extension Acronym: Migration {
     static func prepare(on connection: PostgreSQLConnection) -> Future<Void> {
         return Database.create(self, on: connection, closure: { (builder) in
             try addProperties(to: builder)
-            try builder.reference(from: \.userID, to: \User.id)
+            try builder.addReference(from: \.userID, to: \User.id)
         })
     }
 }
